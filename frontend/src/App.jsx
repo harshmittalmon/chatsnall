dfe9d812398f45f65aad662e82fb1e87ac449491
harshmittalmon <harshmittalmon@gmail.com>
2025-12-08 10:22:49 +0530

    signup page and functionality made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Route, Routes } from "react-router";
-import ChatPage from "./pages/ChatPage.jsx"
+import React, { useEffect } from 'react';
+import { Navigate, Route, Routes } from "react-router";
+import ChatPage from "./pages/ChatPage.jsx";
 import LoginPage from './pages/LoginPage.jsx';
 import SignUpPage from "./pages/SignUpPage.jsx";
-import {useAuthStore} from './store/useAuthStore.js';
-  
+import { useAuthStore } from './store/useAuthStore.js';
+import PageLoader from './components/PageLoader.jsx';
+import {Toaster} from "react-hot-toast";
 export default function App() {
-
-  const {authUser,login,logout,isLoggedIn} = useAuthStore();
+      const {checkAuth, isCheckingAuth,authUser} = useAuthStore();
+      useEffect(()=>{
+        checkAuth()
+      },[checkAuth]);
+      console.log("authUser:",authUser);
+      if( isCheckingAuth ) return <PageLoader/>;
       return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
       {/* DECORATORS - GRID BG & GLOW SHAPES */}
@@ -16,15 +21,13 @@ export default function App() {
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
 
-      <button onClick={login} className='z-10'> Login </button>
-      <button onClick={logout} className='z-10'> Logout</button>
 
       <Routes>
-        <Route path="/" element={ <ChatPage /> } />
-        <Route path="/login" element={<LoginPage /> } />
-        <Route path="/signup" element={ <SignUpPage />} />
+        <Route path="/" element={ authUser?  <ChatPage /> : <Navigate to={"/login"}/>} />
+        <Route path="/login" element={ !authUser? <LoginPage /> : <Navigate to={"/"}/> } />
+        <Route path="/signup" element={ !authUser? <SignUpPage /> : <Navigate to={"/"}/>  } />
       </Routes>
-
+        <Toaster/>
     </div>
   );
 }
